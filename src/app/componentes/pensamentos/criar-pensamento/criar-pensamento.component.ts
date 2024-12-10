225a29156a6d3200538c7c3b7a8c801c960d90b2
Lucas Bailo <lucasebailo@gmail.com>
2024-12-09 22:10:36 -0300

    adding CRUD components

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id:'1',
+    conteudo: '',
+    autoria: '',
+    modelo:'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ciarPensamento() {
+    alert("OIE")
+  }
+
+  cancelar() {
+    alert("tchau")
+  }
+
+}

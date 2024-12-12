9624761e2093b7b4d55f3af572322fd027fdff7e
Lucas Bailo <lucasebailo@gmail.com>
2024-12-12 14:50:33 -0300

    adding json server and service.ts

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: '256 char test class: sadkfjasdfljsdfjsadlfjasdçfmasdlançasdlfndasçlkfasdklfsdafklasdfdlçaskfnasdçklnç ldnvaldkfndsalçkfnasdlçkfaasdlfndasçlkfasdklfsdafklasdfdlçaskfnasdçklnç ldnvaldkfndsalçkfnasdlçkfaasdlfndasçlkfasdklfsdafklasdfdlçaskfnasdçklnç ldnvaldkfndsalçkfnasdlçkfaasdlfndasçlkfasdklfsdafklasdfdlçaskfnasdçklnç ldnvaldkfndsalçkfnasdlçkfasdçflkdsnav ççadfvnçvn vkjsdf çkjv dfçkjv sdfçkjv sdfçkjv dsfçkjvsdfçkjv sdkjv sdfvk vçdskv ',
-      autoria: 'Componente filho',
-      modelo: 'modelo1'
-    }
-  ]
+  listaPensamentos: Pensamento[] = []
 
   constructor() { }
 
